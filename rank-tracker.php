193b6dcf8383432411bb81200d5198297e858eec
Çağdaş Dağ <cagdasdag81@gmail.com>
2024-02-13 10:22:10 +0300

    Alternate cron flag removed

--- rank-tracker.php
+++ rank-tracker.php
@@ -41,7 +41,6 @@ define('WPRT_API_RANK_URL', WPRT_API_URL . '/wp-json/wprta/v1/api/rank');
 define('WPRT_API_REMOVE_LICENSE_URL', WPRT_API_URL . '/wp-json/wprta/v1/api/remove-license?licenseKey=%s');
 define('WPRT_INVALID_LICENSE_KEY_MESSAGE', 'The license key is not valid.');
 define('WPRT_DAILY_USAGE_LIMIT_EXPIRED', 'Daily Usage Limit expired!');
-define('ALTERNATE_WP_CRON', true);
 define('WPRT_SUPPORT_LINK', 'https://wpranktracker.com/support');
 
 require_once __DIR__ . '/init.php';
